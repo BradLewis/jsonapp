5d91078feab97a65457d43e37bdf039014684eef
BradLewis <blewis2109@gmail.com>
2016-12-04 22:24:40 +1100

    Trying again

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(function (req, res, next) {
-    res.contentType('application/json');
+    res.set('Content-Type', 'application/json');
     next();
   });
 
